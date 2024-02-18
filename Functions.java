2906fb3dc9d7986d47ef19a24780745ec1d932c3
Ronit Kene <97221143+Ig-Ronit@users.noreply.github.com>
2024-02-19 01:53:21 +0530

    Functions.java

--- Functions.java
+++ Functions.java
@@ -1,10 +1,10 @@
 package Functions;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class test_fun1 {
 	
-//	1st
+//	1st no
 	public  void printAverage(){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Three Number");
@@ -71,9 +71,80 @@ public class test_fun1 {
 		} while (n > 0);
 		
 	}
+//	7th
+	public void findRaiseValue() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number");
+		int n = sc.nextInt();
+		System.out.println("Enter Its Power");
+		int p = sc.nextInt();
+		int val = 1;
+		for(int i =1; i<=p; i++) { 
+			val = val*n;
+		}
+		System.out.println("the value of "+n+" to the power"+p+" is "+val);
+	}
+//	8th
+	public void fibonacci () {
+		Scanner sc = new Scanner(System.in);
+		int a=1,b=0,fib=0;
+		System.out.println("Enter The Number Upto Which you want the Fibonacci Series");
+		int n =sc.nextInt();
+		for(int i = 1 ; i<=n;i++) {
+			System.out.print(fib+" ");
+			fib =a+b;
+			a=b;
+			b=fib;
+			
+		}
+	}
+//	9th
+	public void findGCD() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Any Two Number to Find its GCD");
+		int n1= sc.nextInt();
+		int n2= sc.nextInt();
+		while(n1 != n2) { 
+	           if(n1>n2) {
+	               n1 = n1 - n2;
+	           } else {
+	               n2 = n2 - n1;
+	           }
+	       }
+	       System.out.println("GCD is : "+ n1);
+		
+	}
+//	10th
+	public void findSigns() {
+	       int positive = 0, negative = 0, zeros = 0;
+	       System.out.println("Press 1 to continue & 0 to stop");
+	       Scanner sc = new Scanner(System.in);
+	       int input = sc.nextInt();
+	       while(input == 1) {
+	           System.out.println("Enter your number : ");
+	           int number = sc.nextInt();
+	           if(number > 0) {
+	               positive++;
+	           } else if(number < 0) {
+	               negative++;
+	           } else {
+	               zeros++;
+	           }
+	           System.out.println("Press 1 to continue & 0 to stop");
+	           input = sc.nextInt();
+	       }
+	       System.out.println("Positives : "+ positive);
+	       System.out.println("Negatives : "+ negative);
+	       System.out.println("Zeros : "+ zeros);
+	   }   
+	
+
+	
 	
 	public static void main(String[] args) {
 		test_fun1 obj = new test_fun1();
+		
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What Function You want to Perform ");
 		System.out.println("");
@@ -83,6 +154,10 @@ public class test_fun1 {
 		System.out.println("Enter 4 to find Circumfenrecr of a Circle");
 		System.out.println("Enter 5 to find if you are Eligible for Voting or not");
 		System.out.println("Enter 6 to Caught in Infinite Loop");
+		System.out.println("Enter 7 to Find The Value of One number raise to power of Another");
+		System.out.println("Enter 8 to Find The Fibonacci Series");
+		System.out.println("Enter 9 to Find The GCD Of two Number");
+		System.out.println("Enter 10 to to count Positive , Negative and Zeros from your Number Series");
 		int number = sc.nextInt();
 		switch (number) {
 		case 1: {
@@ -106,8 +181,23 @@ public class test_fun1 {
 			break;
 		}
 		case 6:{
-			obj.infiniteLoop();
+		obj.infiniteLoop();
 			break;
+		}
+		case 7:{
+			obj.findRaiseValue();
+			break;
+		}
+		case 8:{
+			obj.fibonacci();
+			break;
+		}
+		case 9:{
+			obj.findGCD();
+			break;
+		}
+		case 10:{
+			obj.findSigns();
 		}
 		default: {
 			System.out.println("Enter Valid Option");
